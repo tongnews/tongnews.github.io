1b33f74438a1bb3f5fb434e6ca1f7a1cd00733bb
tongnews <tommyzenth@live.cn>
2015-03-05 00:46:58 +1100

    Major Update
    Add JSON connection to slider~

--- js/json/index_post.js
+++ js/json/index_post.js
@@ -5,18 +5,21 @@
 
 $(document).ready(function () {
     console.log("Starting JSON POSTS engine!");
+    updateSlides();
     updatePosts();
+
     var postid = (parseInt(getUrlParam("id"), 8) - 100000) / 9;
     console.log(postid);
     console.log((postid * 9 + 100000).toString(8));
 });
 
-var $post_container = $('#index_post_container'),
-    $post_cells = $post_container.find('.post_cell');
+var baseurl="http://54.92.122.102/wordpress/";
 
 function updatePosts() {
 
-    var questurl = "http://54.92.122.102/wordpress/?json=get_recent_posts";
+    var questurl = baseurl.concat("?json=get_category_posts&category_slug=post");
+    var $post_container = $('#index_post_container'),
+    $post_cells = $post_container.find('.post_cell');
     //ajax for get recent post
     $.ajax({
         url: questurl,
@@ -26,7 +29,9 @@ function updatePosts() {
             format: "json"
         },
         success: function (response) {
-
+            
+            //console.log(response);
+            
             var $post_counts = 7;
 
             for (var i = 0; i < $post_counts; i++) {
@@ -42,6 +47,45 @@ function updatePosts() {
     });
 
 }
+
+
+
+
+function updateSlides() {
+
+    var questurl = baseurl.concat("?json=get_category_posts&category_slug=slide");
+    //ajax for get recent post
+    var $slide_container = $('#sl-slider'),
+    $slide_cells = $slide_container.find('.sl-slide');
+    $.ajax({
+        url: questurl,
+        jsonp: "callback",
+        dataType: "jsonp",
+        data: {
+            format: "json"
+        },
+        success: function (response) {
+            
+            //console.log(response);
+            
+            var $slide_counts = 5;
+
+            for (var i = 0; i < $slide_counts; i++) {
+                //replace title
+                $slide_cells.eq(i).find('p').text(response.posts[0].title);
+                //replace intro
+                $slide_cells.eq(i).find('h2').text(response.posts[0].custom_fields.series[0]);
+                var $tbnlurl = response.posts[0].attachments[0].images.full.url;
+                $slide_cells.eq(i).find('.bg-img-'.concat(i+1)).css(
+                    'background-image', 'url('+$tbnlurl+')'
+                );
+            }
+        }
+
+    });
+
+}
+
 
 //---------------------------------json tutorial---------------------------------------
 //    <p id="demo"></p>
