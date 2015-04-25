bf7f73a06065f835f14f215d94d8c011c9fee922
tongnews <tommyzenth@live.cn>
2015-04-25 16:18:30 +1000

    Major Change
    Refacotr Reusage JS code
    Fix bug of bk accessary with responsive screen effect

--- js/jQuery/unitool.js
+++ js/jQuery/unitool.js
@@ -1,25 +1,22 @@
+//-------------------------Resources Contronller----------------//
+
+//Url Control
 function getBaseUrl() {
     return "http://54.92.122.102/wordpress/";
 }
-
 function getRegBaseUrl() {
     return /http:\/\/54.92.122.102\/wordpress\//g;
 }
-
 function getBkdomainUrl() {
     return "http://bk.tongnews.org/wordpress/";
 }
-
 function getCDNUrl() {
     return "http://7xi53n.com1.z0.glb.clouddn.com/";
 }
-
-
 function getDomain() {
     console.log(document.domain);
     return document.domain;
 }
-
 function getUrlParam(sParam) {
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
@@ -30,29 +27,7 @@ function getUrlParam(sParam) {
         }
     }
 }
-
-var $videolinkpref = "tudo";
-
-function getVideoLinkref() {
-    return $videolinkpref;
-}
-
-function setVideoLinkref(ref) {
-    $videolinkpref = ref;
-}
-
-Array.prototype.contains = function (element) {
-    return this.indexOf(element) > -1;
-};
-
-function pageScroll() {
-    window.scrollBy(0, -800);
-    scrolldelay = setTimeout('pageScroll()', 1000);
-    if (document.documentElement.scrollTop == 0) clearTimeout(scrolldelay);
-}
-
 function encodeId(postid) {
-
 //    console.log(postid);
 //    var x = postid;
 //    var y = x.toString();
@@ -65,22 +40,29 @@ function encodeId(postid) {
     return postid;
     //return encodedString = Base64.encode(postid+12345);
 }
-
 function decodeIdfromAddr() {
-
 //    var x = getUrlParam("id") ^ 942857;
 //    var y = "0000000000"+x.toString();
 //    var y = y.substr(y.length-6);
 //    var z = y.substring(y.length / 2);
 //    console.log(y);
 //    console.log(Number(z));
-    
     return Number(getUrlParam("id"));
-
     //var postid = Base64.decode(getUrlParam("id"))-12345;
     //return postid;
 }
 
+//Video referrence
+var $videolinkpref = "tudo";
+function getVideoLinkref() {
+    return $videolinkpref;
+}
+function setVideoLinkref(ref) {
+    $videolinkpref = ref;
+}
+Array.prototype.contains = function (element) {
+    return this.indexOf(element) > -1;
+};
 function videorefanlayse(refstr) {
     var $linkstr = refstr.split("$");
     var $videolink = [];
@@ -99,13 +81,29 @@ function videorefanlayse(refstr) {
     return $videolink;
 }
 
-//user management
-var managerLogin = false;
 
+
+
+
+
+
+
+//-------------------------Animation Contronller ----------------//
+function pageScroll() {
+    window.scrollBy(0, -800);
+    scrolldelay = setTimeout('pageScroll()', 1000);
+    if (document.documentElement.scrollTop == 0) clearTimeout(scrolldelay);
+}
+
+
+
+
+
+//-------------------------User Contronller --------------------//
+var managerLogin = false;
 function getManagerlogin() {
     return managerLogin;
 }
-
 function getCookie() {
     if (readCookie("user")) {
         return readCookie("user");
@@ -113,7 +111,6 @@ function getCookie() {
         return -1;
     }
 }
-
 function checkCookie() {
     //user handler
     var curcookie = getCookie();
@@ -154,14 +151,12 @@ function checkCookie() {
         $(".login_container").attr("style", "");
     }
 }
-
 function sucessLogin(response) {
     $(".login_container").attr("style", "width:0px; height:0px; overflow:hidden;");
     $(".logined_container").attr("style", "");
     managerLogin = true;
     document.getElementById("user_welcome").innerHTML = "&nbsp; 欢迎您 | " + response.nickname +"&nbsp;";
 }
-
 function createCookie(name, value, days) {
     if (days) {
         var date = new Date();
@@ -170,7 +165,6 @@ function createCookie(name, value, days) {
     } else var expires = "";
     document.cookie = name + "=" + value + expires + "; path=/";
 }
-
 function readCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
@@ -181,8 +175,193 @@ function readCookie(name) {
     }
     return null;
 }
-
 function logTimeNow(text){
     var time=new Date();
     console.log(text+':'+time.getMinutes()+':'+time.getSeconds());
+}
+
+
+
+
+
+//-------------------------Content Contronller ----------------//
+var $curCount = 0,
+    $maxPages = 0;
+
+function postArranger(response,postCount,source){
+    
+    $curCount = response.count;
+    $maxPages = response.pages;
+
+    switch(source){
+            case "index.html":
+                $(".post_cell_container").find(".post_cell").remove();
+                $(".post_cell_container").find(".postslider").remove();
+                break;
+    }
+   
+    //set post basic title info and other
+    for (var i = 0; i < $curCount; i++) {
+        
+        if(i<($curCount-1)){
+            var postHtml=$('<div class="post_cell uniborder"><img><blockquote><a target="_blank"></a><li></li><tags><li id="tagend"></li></tags><p></p></blockquote></div><div class="postslider"></div>');
+        }else{
+            var postHtml=$('<div class="post_cell uniborder"><img><blockquote><a target="_blank"></a><li></li><tags><li id="tagend"></li></tags><p></p></blockquote></div>');
+        }
+           
+        //replace title
+        postHtml.find('a').text(response.posts[i].title.substring(0, 31));
+        postHtml.find('a').attr("href", "tongpost.html?id=" + encodeId(response.posts[i].id));
+        //replace info
+        try {
+            var flt_comment_count = response.posts[i].custom_fields.float_comment[0].split('$').length - 1;
+        } catch (err) {
+            var flt_comment_count = 0;
+        }
+        postHtml.find('li')[0].innerHTML = "<i class='fa fa-pencil fa-fw'></i>&nbsp;" + response.posts[i].author.nickname + " <i class='fa fa-clock-o fa-fw'></i>&nbsp;" + response.posts[i].date.substring(0, 10) + " <i class='fa fa-eye fa-fw'></i>&nbsp;" + response.posts[i].custom_fields.viewer_count[0] + " <i class='fa fa-comment fa-fw'></i>&nbsp;" + flt_comment_count;
+        //replace intro
+        postHtml.find('p').text(response.posts[i].custom_fields.intro[0]);
+        var $tbnlurl = response.posts[i].custom_fields.thumbnail_url[0].replace(baseurl, cdnurl);
+        $tbnlurl = $tbnlurl.replace(bkurl, cdnurl);
+
+        postHtml.find('img').attr('src', $tbnlurl);
+
+        //remove addional attribute
+        postHtml.find(".float_video_link").remove();
+
+        //check if it is a video
+        var $postCategories = response.posts[i].categories;
+        for (var j = 0; j < $postCategories.length; j++) {
+            if ($postCategories[j].slug == "video") {
+                var $video_link = response.posts[i].custom_fields.video_link[0];
+                $("<div class='float_video_link' ref='" + $video_link + "' post_id=" + response.posts[i].id + "></div>").insertAfter(postHtml.find('p'));
+                break;
+            }
+        }
+
+        //add tages into post
+        postHtml.find(".tag").remove();
+        var $postTags = response.posts[i].tags;
+        for (var j = 0; j < $postTags.length; j++) {
+            var tagstyle='style="background-color:#FD7C98"';
+            switch ($postTags[j].group) {
+              case "not assigned":
+                  tagstyle = 'style="background-color:#808080"';
+                  break;
+              case "else":
+                  tagstyle = 'style="background-color:#FD7C98"';
+                  break;
+              case "area":
+                  tagstyle = 'style="background-color:#rgb(255, 41, 181)"';
+                  break;
+              case "origin":
+                  tagstyle = 'style="background-color:#bc7cfd"';
+                  break;
+              case "character":
+                  tagstyle = 'style="background-color:#1ebbd0"';
+                  break;
+              case "activity":
+                  tagstyle = 'style="background-color:#37d078"';
+                  break;
+          };
+            $("<li class='tag' "+tagstyle+">" + $postTags[j].title + "</li>").insertBefore(postHtml.find('#tagend'));
+        }
+
+        
+        postHtml.appendTo('.post_cell_container');
+    }
+    
+    //control click event of tag searching
+    $('.tag').click(function () {
+          window.open('search.html?type=t&key='+$(this).text());
+    });
+
+    //control click event of float video button
+    $('.float_video_link').click(function () {
+        var questurl = baseurl.concat("?json=view_post&id=" + $(this).attr('post_id'));
+        //ajax for add viewer_count for this post
+        $.ajax({
+            url: questurl,
+            jsonp: "callback",
+            dataType: "jsonp",
+            data: {
+                format: "json"
+            },
+            success: function (response) {
+                console.log("viewing post " + response.post.title);
+            }
+        });
+        //connect to post video frame
+        var $videolink = videorefanlayse($(this).attr('ref'));
+        //console.log($(this).attr('ref'));
+        var linktype = getVideoLinkref();
+        if (typeof $videolink[linktype] == 'undefined') {
+            linktype = $videolink["avaliable"];
+        }
+        if (linktype == "tudo") {
+            $('#index_float_video_ply').find('iframe').eq(0).attr("style", "width:0px; height:0px; overflow:hidden;");
+            $('#index_float_video_ply').find('embed').eq(0).attr("style", "width:0px; height:0px; overflow:hidden;");
+            $('#index_float_video_ply').find('iframe').replaceWith($videolink[linktype]);
+            $('#index_float_video_ply').find('iframe').eq(0).attr("style", "");
+        }
+        if (linktype == "bilibili") {
+            $('#index_float_video_ply').find('iframe').eq(0).attr("style", "width:0px; height:0px; overflow:hidden;");
+            $('#index_float_video_ply').find('embed').eq(0).attr("style", "width:0px; height:0px; overflow:hidden;");
+            $('#index_float_video_ply').find('embed').replaceWith($videolink[linktype]);
+            $('#index_float_video_ply').find('embed').eq(0).attr("width", "");
+            $('#index_float_video_ply').find('embed').eq(0).attr("height", "");
+        }
+        $('#index_float_video_ply').attr("style", "right: " + $fly_video_right);
+    });
+    
+    $('.loading_cover').attr("style","width:0px; height:0px; overflow:hidden;");
+
+    for (var i = $curCount; i < postCount; i++) {
+        $post_cells.eq(i).attr("style", "width:0px; height:0px; overflow:hidden;");
+    }
+
+    //control click event of tag searching
+    $('.tag').click(function () {
+          window.open('search.html?type=t&key='+$(this).text());
+    });
+
+    //control click event of float video button
+    $('.float_video_link').click(function () {
+        var questurl = baseurl.concat("?json=view_post&id=" + $(this).attr('post_id'));
+        //ajax for add viewer_count for this post
+        $.ajax({
+            url: questurl,
+            jsonp: "callback",
+            dataType: "jsonp",
+            data: {
+                format: "json"
+            },
+            success: function (response) {
+                console.log("viewing post " + response.post.title);
+            }
+        });
+        //connect to post video frame
+        var $videolink = videorefanlayse($(this).attr('ref'));
+        //console.log($(this).attr('ref'));
+        var linktype = getVideoLinkref();
+        if (typeof $videolink[linktype] == 'undefined') {
+            linktype = $videolink["avaliable"];
+        }
+        if (linktype == "tudo") {
+            $('#index_float_video_ply').find('iframe').eq(0).attr("style", "width:0px; height:0px; overflow:hidden;");
+            $('#index_float_video_ply').find('embed').eq(0).attr("style", "width:0px; height:0px; overflow:hidden;");
+            $('#index_float_video_ply').find('iframe').replaceWith($videolink[linktype]);
+            $('#index_float_video_ply').find('iframe').eq(0).attr("style", "");
+        }
+        if (linktype == "bilibili") {
+            $('#index_float_video_ply').find('iframe').eq(0).attr("style", "width:0px; height:0px; overflow:hidden;");
+            $('#index_float_video_ply').find('embed').eq(0).attr("style", "width:0px; height:0px; overflow:hidden;");
+            $('#index_float_video_ply').find('embed').replaceWith($videolink[linktype]);
+            $('#index_float_video_ply').find('embed').eq(0).attr("width", "");
+            $('#index_float_video_ply').find('embed').eq(0).attr("height", "");
+        }
+        $('#index_float_video_ply').attr("style", "right: " + $fly_video_right);
+    });
+
+    logTimeNow('PostJSON end');
 }
--- js/json/index_service.js
+++ js/json/index_service.js
@@ -1,17 +1,13 @@
 /**
  * Created by tchann 3/3/2015
  */
-
-
-var $postCount = 7,
-    $pageNum = 1;
-var $rankCount = 10;
 var baseurl = getBaseUrl();
 var cdnurl = getCDNUrl();
 var bkurl = getBkdomainUrl();
 
-var $curCount = 0,
-    $maxPages = 0;
+var $postCount = 7,
+    $pageNum = 1;
+var $rankCount = 10;
 var $fly_video_right = 20;
 
 $(document).ready(function () {
@@ -29,8 +25,7 @@ $(document).ready(function () {
     //user management
     //createCookie("user","",14);
     checkCookie();
-    
-     
+ 
 });
 
 function scrollbarCustom(){
@@ -38,11 +33,9 @@ function scrollbarCustom(){
 }
 
 function updatePosts(postCount, pageNum) {
+    
     $('.loading_cover').attr("style", "");
     var questurl = baseurl.concat("?json=get_category_posts_main&category_slug=post&count=" + postCount + "&page=" + pageNum);
-    var $post_container = $('#index_post_container'),
-        $post_cells = $post_container.find('.post_cell');
-
     //ajax for get recent post
     logTimeNow('PostJSON start');
     
@@ -54,132 +47,13 @@ function updatePosts(postCount, pageNum) {
             format: "json"
         },
         success: function (response) {
-            
-            logTimeNow('PostJSON back');
-            
-            $curCount = response.count;
-            $maxPages = response.pages;
-
-            //set post basic title info and other
-            for (var i = 0; i < $curCount; i++) {
-                $post_cells.eq(i).attr("style", "");
-                //replace title
-                $post_cells.eq(i).find('a').text(response.posts[i].title.substring(0, 31));
-                $post_cells.eq(i).find('a').attr("href", "tongpost.html?id=" + encodeId(response.posts[i].id));
-                //replace info
-                try {
-                    var flt_comment_count = response.posts[i].custom_fields.float_comment[0].split('$').length - 1;
-                } catch (err) {
-                    var flt_comment_count = 0;
-                }
-                $post_cells.eq(i).find('li')[0].innerHTML = "<i class='fa fa-pencil fa-fw'></i>&nbsp;" + response.posts[i].author.nickname + " <i class='fa fa-clock-o fa-fw'></i>&nbsp;" + response.posts[i].date.substring(0, 10) + " <i class='fa fa-eye fa-fw'></i>&nbsp;" + response.posts[i].custom_fields.viewer_count[0] + " <i class='fa fa-comment fa-fw'></i>&nbsp;" + flt_comment_count;
-                //replace intro
-                $post_cells.eq(i).find('p').text(response.posts[i].custom_fields.intro[0]);
-                var $tbnlurl = response.posts[i].custom_fields.thumbnail_url[0].replace(baseurl, cdnurl);
-                $tbnlurl = $tbnlurl.replace(bkurl, cdnurl);
-                
-                $post_cells.eq(i).find('img').attr('src', $tbnlurl);
-
-                //remove addional attribute
-                $post_cells.eq(i).find(".float_video_link").remove();
-
-                //check if it is a video
-                var $postCategories = response.posts[i].categories;
-                for (var j = 0; j < $postCategories.length; j++) {
-                    if ($postCategories[j].slug == "video") {
-                        var $video_link = response.posts[i].custom_fields.video_link[0];
-                        $("<div class='float_video_link' ref='" + $video_link + "' post_id=" + response.posts[i].id + "></div>").insertAfter($post_cells.eq(i).find('p'));
-                        break;
-                    }
-                }
-
-                //add tages into post
-                $post_cells.eq(i).find(".tag").remove();
-                var $postTags = response.posts[i].tags;
-                for (var j = 0; j < $postTags.length; j++) {
-                    var tagstyle='style="background-color:#FD7C98"';
-                    switch ($postTags[j].group) {
-                      case "not assigned":
-                          tagstyle = 'style="background-color:#808080"';
-                          break;
-                      case "else":
-                          tagstyle = 'style="background-color:#FD7C98"';
-                          break;
-                      case "area":
-                          tagstyle = 'style="background-color:rgb(255, 41, 181)"';
-                          break;
-                      case "origin":
-                          tagstyle = 'style="background-color:#bc7cfd"';
-                          break;
-                      case "character":
-                          tagstyle = 'style="background-color:#1ebbd0"';
-                          break;
-                      case "activity":
-                          tagstyle = 'style="background-color:#37d078"';
-                          break;
-                  };
-                    $("<li class='tag' "+tagstyle+">" + $postTags[j].title + "</li>").insertBefore($post_cells.eq(i).find('#tagend'));
-                }
-
-            }
-            
-            $('.loading_cover').attr("style", "width:0px; height:0px; overflow:hidden;");
-            
-            for (var i = $curCount; i < postCount; i++) {
-                $post_cells.eq(i).attr("style", "width:0px; height:0px; overflow:hidden;");
-            }
-            
-            //control click event of tag searching
-            $('.tag').click(function () {
-                  window.open('search.html?type=t&key='+$(this).text());
-            });
-            
-            //control click event of float video button
-            $('.float_video_link').click(function () {
-                var questurl = baseurl.concat("?json=view_post&id=" + $(this).attr('post_id'));
-                //ajax for add viewer_count for this post
-                $.ajax({
-                    url: questurl,
-                    jsonp: "callback",
-                    dataType: "jsonp",
-                    data: {
-                        format: "json"
-                    },
-                    success: function (response) {
-                        console.log("viewing post " + response.post.title);
-                    }
-                });
-                //connect to post video frame
-                var $videolink = videorefanlayse($(this).attr('ref'));
-                //console.log($(this).attr('ref'));
-                var linktype = getVideoLinkref();
-                if (typeof $videolink[linktype] == 'undefined') {
-                    linktype = $videolink["avaliable"];
-                }
-                if (linktype == "tudo") {
-                    $('#index_float_video_ply').find('iframe').eq(0).attr("style", "width:0px; height:0px; overflow:hidden;");
-                    $('#index_float_video_ply').find('embed').eq(0).attr("style", "width:0px; height:0px; overflow:hidden;");
-                    $('#index_float_video_ply').find('iframe').replaceWith($videolink[linktype]);
-                    $('#index_float_video_ply').find('iframe').eq(0).attr("style", "");
-                }
-                if (linktype == "bilibili") {
-                    $('#index_float_video_ply').find('iframe').eq(0).attr("style", "width:0px; height:0px; overflow:hidden;");
-                    $('#index_float_video_ply').find('embed').eq(0).attr("style", "width:0px; height:0px; overflow:hidden;");
-                    $('#index_float_video_ply').find('embed').replaceWith($videolink[linktype]);
-                    $('#index_float_video_ply').find('embed').eq(0).attr("width", "");
-                    $('#index_float_video_ply').find('embed').eq(0).attr("height", "");
-                }
-                $('#index_float_video_ply').attr("style", "right: " + $fly_video_right);
-            });
-            
-            logTimeNow('PostJSON end');
+            postArranger(response,postCount,location.href.split("/").slice(-1)[0]);
         }
 
     });
 
 }
 
-//control the post
 $('.nextPostPage').click(function () {
     $pageNum = $pageNum + 1;
     if ($pageNum >= ($maxPages - 1)) {
@@ -352,7 +226,7 @@ $('.search_button').click(function () {
 });
 
 
-//----------------------------------     user login ----------------------------------------
+//----------------------------------  user login ----------------------------------------
 $('.user_login').click(function () {
     var questurl = baseurl.concat("api/user/generate_auth_cookie/?username=" + document.getElementById('user_name_input').value + "&password=" + document.getElementById('user_pass_input').value);
     $(this).css('background','rgba(102, 251, 154, 0.67)');
@@ -457,36 +331,3 @@ document.getElementById('user_sginin_nickname_input').value + "&nonce=" + $user_
         }
     });
 });
-
-//---------------------------------json tutorial---------------------------------------
-//    <p id="demo"></p>
-//    <script>
-//    var text = '{"employees":[' +
-//    '{"firstName":"John","lastName":"Doe" },' +
-//    '{"firstName":"Anna","lastName":"Smith" },' +
-//    '{"firstName":"Peter","lastName":"Jones" }]}';
-//
-//    obj = JSON.parse(text);
-//    document.getElementById("demo").innerHTML =
-//    obj.employees[1].firstName + " " + obj.employees[2].lastName;
-//    </script>
-//
-// Tell YQL what we want and that we want JSON
-//    data: {
-//        q: "select title,abstract,url from search.news where query=\"cat\"",
-//        format: "json"
-//    },
-
-//ajax for get cookie
-//    var questurl = baseurl.concat("api/user/generate_auth_cookie/?username=&password=");
-//    $.ajax({
-//        url: questurl,
-//        jsonp: "callback",
-//        dataType: "jsonp",
-//        data: {
-//            format: "json"
-//        },
-//        success: function (response) {
-//            console.log(response);
-//        }
-//    });
