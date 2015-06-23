0910dfa4c3e4409f6841102e6af1f17c97370dca
tongnews <tommyzenth@live.cn>
2015-06-23 15:46:42 +1000

    Minor Update
    Fix bug

--- tongpost.php
+++ tongpost.php
@@ -11,10 +11,7 @@ if(file_exists($filepath)){
 $json_data = json_decode($json, true);
 $php_title = $json_data['posts'][0]['title'];
 $php_content= $json_data['posts'][0]['content'];
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
 ?> 
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
